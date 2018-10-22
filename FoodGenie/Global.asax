0adb23c91951fd886490371a7fbc110f1760d042
Anmol Vashistha <anmol.vashistha@newgen.co.in>
2018-10-22 17:14:11 +0530

    Add project files.

---
+++ FoodGenie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodGenie.MvcApplication" Language="C#" %>
---
+++ FoodGenie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodGenie.Startup))]
+namespace FoodGenie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
