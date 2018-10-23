938f7de82c3941bf30cded25c250ac4c6044a572
Anmol Vashistha <anmol.vashistha@newgen.co.in>
2018-10-23 12:34:53 +0530

    Added migrations for fname and lname to register-viewmodel and identity-model

--- FoodGenie/Models/AccountViewModels.cs
+++ FoodGenie/Models/AccountViewModels.cs
@@ -65,6 +65,13 @@ namespace FoodGenie.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
